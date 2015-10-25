19e14a7b072e1dc508f70628a171eb164761dd70
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-10-25 20:00:50 +0000

    add mocha opts & change db to mongolabs remote

--- api/config.js
+++ api/config.js
@@ -1,6 +1,8 @@
 module.exports = {
 
   db: {
-    url: process.env.URL || require("./creds.json").db.url
+    url: process.env.URL || require("./creds.json").db.url,
+    user: process.env.DB_USER || require("./creds.json").db.db_user,
+    password: process.env.PASSWORD || require("./creds.json").db.password
   }
 }
