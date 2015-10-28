f52065d6405fc7e9401feaec4a0e2f1396ab00e8
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-10-28 23:30:26 +0000

    trimmed unnecessary lines, prepped for presenting

--- api/config.js
+++ api/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   db: {
-    url: process.env.URL || require("./creds.json").db.url,
-    user: process.env.DB_USER || require("./creds.json").db.db_user,
+    url: require("./creds.json").db.url,
+    user: process.env.DB_USER || require("./creds.json").db.user,
     password: process.env.PASSWORD || require("./creds.json").db.password
   }
 }
