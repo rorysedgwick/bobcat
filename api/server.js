69560ce4752fc57690ea9d3eaab961536ccfd5c9
Rory Sedgwick <rory.sedgwick@gmail.com>
2015-10-29 23:13:06 +0000

    updated server to use __dirname

--- api/server.js
+++ api/server.js
@@ -2,14 +2,14 @@
 
 var express = require("express");
 var router = require("./routes/routes.js");
+var path = require("path");
 var app = express();
 var port = Number(process.env.PORT) || 8000;
 
 app.use(require("body-parser").json({limit: "52428800"}));
 app.use("/", router);
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 app.listen(port,
   console.log("server listening at port 8000"
 ));
-
